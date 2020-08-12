f383f4c5828531b730d92dedc91b19a888a8a3f2
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-12 19:26:53 +0800

    Organise files

--- cifar40_scripts/cifar40_exp_cl_wcl.sh
+++ cifar40_scripts/cifar40_exp_cl_wcl.sh
@@ -45,13 +45,13 @@ cd ..
 #python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 5
 #python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 3 --select 5
 #
-python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 5
-python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 5
-python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 3 --select 5
+#python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 5
+#python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 5
+#python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 3 --select 5
 
-#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 6
-#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 6
-#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 6
+python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 6
+python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 6
+python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 6
 #
 #python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 6
 #python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 6
