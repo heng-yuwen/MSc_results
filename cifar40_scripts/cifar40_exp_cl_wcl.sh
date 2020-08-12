106723c13b33a514db867ee587b11b63cd656138
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-12 19:17:48 +0800

    Organise files

--- cifar40_scripts/cifar40_exp_cl_wcl.sh
+++ cifar40_scripts/cifar40_exp_cl_wcl.sh
@@ -37,13 +37,13 @@ export DATASET_DIR=${TMP}/datasets/
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
 
-python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 5
-python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
-python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 5
+#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 5
+#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
+#python cifar40_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 5
 #
-#python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 5
-#python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 5
-#python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 3 --select 5
+python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 5
+python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 5
+python cifar40_experiments.py --experiment 5 --batch_size 256  --stage 3 --select 5
 #
 #python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 5
 #python cifar40_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 5
