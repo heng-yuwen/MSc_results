90b43fe4600d1357ed89621befda32bf9e46e8b5
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-13 15:37:18 +0800

    Second part

--- cifar100_scripts/cifar100_exp_cl_wcl.sh
+++ cifar100_scripts/cifar100_exp_cl_wcl.sh
@@ -38,7 +38,7 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
 
 #python cifar100_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 5
-#python cifar100_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
+python cifar100_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
 #python cifar100_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 5
 #
 #python cifar100_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 5
--- cifar10_scripts/cifar10_exp_cl_wcl.sh
+++ cifar10_scripts/cifar10_exp_cl_wcl.sh
@@ -38,7 +38,7 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
 
 #python cifar10_experiments.py --experiment 4 --batch_size 256  --stage 1 --select 5
-#python cifar10_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
+python cifar10_experiments.py --experiment 4 --batch_size 256  --stage 2 --select 5
 #python cifar10_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 5
 #
 #python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 5
@@ -93,6 +93,6 @@ cd ..
 #python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 9
 #python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 3 --select 9
 #
-python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 9
+#python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 9
 #python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 9
 #python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 3 --select 9
