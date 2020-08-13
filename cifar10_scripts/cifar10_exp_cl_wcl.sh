431d8e438d33b89493fa4cd000cf82181ca46daa
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-13 15:59:30 +0800

    Second part

--- cifar10_scripts/cifar10_exp_cl_wcl.sh
+++ cifar10_scripts/cifar10_exp_cl_wcl.sh
@@ -90,9 +90,9 @@ cd ..
 #python cifar10_experiments.py --experiment 4 --batch_size 256  --stage 3 --select 9
 #
 #python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 1 --select 9
-python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 9
+#python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 2 --select 9
 #python cifar10_experiments.py --experiment 5 --batch_size 256  --stage 3 --select 9
 #
 #python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 1 --select 9
-#python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 9
+python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 2 --select 9
 #python cifar10_experiments.py --experiment 6 --batch_size 256  --stage 3 --select 9
