44e6f95cd078ba656cf491a824fabf8a7c6a14ba
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-16 16:39:40 +0800

    Update his

--- trade_off_collect_data.py
+++ trade_off_collect_data.py
@@ -29,6 +29,7 @@ print("Train with the im wcl selected dataset.")
 history = collect_wcl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), None, "cifar10", 10,
                   batch_size=batch_size, )
 for his in history:
+    print(his)
     np.save(os.path.join(os.getcwd(), "models", "cifar10", "framework",
                          "im_wcl_his_size_" + str(his["size"]) + ".npy"), history)
 print("History saved.")
