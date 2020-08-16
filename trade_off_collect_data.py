e9469fb7a438552334c1f778f6a1c39141ad1302
Yuwen Heng <hengyuwen_alex@163.com>
2020-08-16 17:16:01 +0800

    Update his

--- trade_off_collect_data.py
+++ trade_off_collect_data.py
@@ -7,16 +7,16 @@ import numpy as np
 from lib.densenet import DenseNet121
 from lib.experiments import load_dataset, collect_wcl
 
-# x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset("cifar10")
-# print("cifar10 loaded")
-#
-# print("There are {} training samples and {} validation samples".format(x_train.shape[0], x_valid.shape[0]))
-# print("There are {} test samples.".format(x_test.shape[0]))
+x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset("cifar10")
+print("cifar10 loaded")
 
-x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset("cifar100")
-print("cifar100 loaded")
 print("There are {} training samples and {} validation samples".format(x_train.shape[0], x_valid.shape[0]))
 print("There are {} test samples.".format(x_test.shape[0]))
+
+# x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset("cifar100")
+# print("cifar100 loaded")
+# print("There are {} training samples and {} validation samples".format(x_train.shape[0], x_valid.shape[0]))
+# print("There are {} test samples.".format(x_test.shape[0]))
 
 
 # batch_size = args.batch_size
@@ -26,9 +26,9 @@ batch_size = 256
 # Experiment 5: train the WCL selected dataset
 
 print("Train with the im wcl selected dataset.")
-history = collect_wcl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), None, "cifar100", 100,
+history = collect_wcl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), None, "cifar10", 10,
                   batch_size=batch_size, )
 for his in history:
-    np.save(os.path.join(os.getcwd(), "models", "cifar100", "framework",
+    np.save(os.path.join(os.getcwd(), "models", "cifar10", "framework",
                          "im_wcl_his_size_" + str(his[0]["size"]) + ".npy"), history[0])
 print("History saved.")
